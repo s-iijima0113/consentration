4e710cb66d5c1d082c1416b1c45fad4dfcd8c695
s-iijima0113 <everyth1ng.n1ce.St01@gmail.com>
2019-11-02 17:19:19 +0900

    トランプ作成２

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+</template>
+
+<script>
+export default {
+  mark : ['heart', 'dia', 'clover', 'spade'],
+  num : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+  surface : ['true', 'false'],
+  for (let i = 0; i < 3; i++ ) {
+    console.log(mark[i])
+  }
+
+  data () {
+    return {      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
