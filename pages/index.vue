3b8607f9a5b223ed9280f8e8aec2cebf77161e6c
s-iijima0113 <everyth1ng.n1ce.St01@gmail.com>
2019-11-02 19:27:38 +0900

    トランプ作成２

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,29 @@
 <template>
+<div class="consentration-box" />
 </template>
 
 <script>
 export default {
-  mark : ['heart', 'dia', 'clover', 'spade'],
-  num : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
-  surface : ['true', 'false'],
-  for (let i = 0; i < 3; i++ ) {
-    console.log(mark[i])
-  }
-
   data () {
-    return {      
+    return {
     }
-  }
+  },
+  methods: {
+    trumps () {
+      const mark = ['heart', 'dia', 'clover', 'spade']
+      console.log(mark)
+      const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+      const surface = ['true', 'false']
+      alert("HEllo World")
+    }
+  }         
 }
 </script>
 
 <style>
-
+.consentration-box{
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+}
 </style>
