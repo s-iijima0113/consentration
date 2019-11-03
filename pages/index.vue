4b1664d46e5a5766c9466f6ecfb2ff4a6733b7df
s-iijima0113 <everyth1ng.n1ce.St01@gmail.com>
2019-11-03 20:37:20 +0900

    トランぷ作成　パターン

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,40 @@
 <template>
-<div class="consentration-box" />
+
 </template>
 
 <script>
 export default {
   data () {
+    const mark = ['heart', 'dia', 'clover', 'spade']
+    const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    const surface = ['true', 'false']
+    const trumps = []
+      for (let i = mark.length -1 ; i > 0; i--) {
+        for (let l = num.length -1 ; l > 0; l--) {
+          const k = Math.floor(Math.random() * (i + 1))
+          const markRandom = mark[i]
+          mark[i] = mark[k]
+          mark[k] = markRandom
+          console.log(mark)
+          // trumps.push(mark[k])
+      
+          const m = Math.floor(Math.random() * (l + 1))
+          const numRandom = num[l]
+          num[l] = num[m]
+          num[m] = numRandom
+          
+        console.log(num)
+        // trumps.push(num)
+        }
+      }
     return {
     }
   },
   methods: {
-    trumps () {
-      const mark = ['heart', 'dia', 'clover', 'spade']
-      console.log(mark)
-      const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-      const surface = ['true', 'false']
-      alert("HEllo World")
-    }
   }         
 }
 </script>
 
 <style>
-.consentration-box{
-  width: 100px;
-  height: 100px;
-  background-color: blue;
-}
+
 </style>
